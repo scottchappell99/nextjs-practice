a85cac186694679c1279bc3c62f5befe18d9cc57
scott.chappell <scott.chappell99@gmail.com>
2025-10-21 18:57:20 -0400

    Fix not needing to do a try catch on deleting invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -102,13 +102,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    try {
-        await sql`DELETE FROM invoices WHERE id=${id}`;
-    } catch (error) {
-        console.error(error);
-        return {message: 'Database Error: Failed to Delete Invoice'};
-    }
-
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
 
